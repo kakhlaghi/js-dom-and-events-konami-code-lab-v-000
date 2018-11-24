ad384895a6ee87ddf928e0e1a4697cefd702d4a9
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-24 20:11:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  function onKeyDownHandler(e)
+  function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which);
+    if(key===alphabet)
+  }
 }
